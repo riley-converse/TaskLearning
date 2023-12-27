474180832ff782fc6c28bc27ea9280ec35112899
Riley Converse <riley.converse@yahoo.com>
2023-12-27 17:16:48 -0500

    Added Testing
    Not completed yet.  Testing the possibility of multiple tasks to handle pattern finding.

--- TaskLearning/Program.cs
+++ TaskLearning/Program.cs
@@ -1,10 +1,86 @@
-﻿namespace TaskLearning
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace TaskLearning
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Asynchronous, doesn't hold up thread.
+            Console.WriteLine("Hello, ");
+            //await PrintConcurrentText();
+            await PrintConcurrentTextAtOnce();
+            Console.WriteLine("World");
+        }
+
+        static async Task PrintConcurrentText()
+        {
+            await Print("First",500);
+            await Print("Second", 500);
+            await Print("Third", 500);
+        }
+
+        static async Task Print(string str, int sleep)
+        {
+            Thread.Sleep(sleep);
+            Console.WriteLine(str);
+            await Task.Yield();
+        }
+        static async Task FindMatch(int numberOfTasks)
+        {
+            
+            using (var cts  = new CancellationTokenSource()) 
+            {
+                Task[] tasks = await TaskFactory(numberOfTasks, cts);
+
+                foreach(Task task in tasks)
+                {
+                    
+                }
+                var completedTask = await Task.WhenAny(tasks);
+               
+                //var result = await completedTask;
+            }
+            
+
+            
+        }
+
+        static async Task<Task[]> TaskFactory(int numerOfTasks, CancellationTokenSource token)
+        {
+            Task[] tasks = new Task[numerOfTasks];
+
+            for (int i = 0;i < numerOfTasks;i++)
+            {
+                await (tasks[i] = new Task(async () => await CheckText( await GenerateChar()),token.Token));
+            }
+
+            return tasks;
+        }
+
+        static async Task<char> GenerateChar()
+        {
+            var rnd = new Random();
+            char c = (char)((byte)rnd.Next(97,122));
+            await Task.Yield();
+            return c;
+        }
+
+        static async Task<bool> CheckText(char targetChar)
+        {
+            string exampleText = "Hello world and everyone who can read this text.";
+            
+            foreach (char c in exampleText)
+            {
+                if (c == targetChar)
+                {
+                    return true;
+                }
+            }
+            return false;
+          
         }
     }
 }
